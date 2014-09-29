729e58d36b31f157e05afe5a9351c6ec1afabfb8
Kelly Morning <kleemorning@gmail.com>
2014-09-29 13:07:27 -0400

    fix typo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ails.application.secrets.email_provider_username,
+    user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
 
