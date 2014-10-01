d43b89c6571f800985ce380fdb3ec333c1abb647
Kelly Morning <kleemorning@gmail.com>
2014-09-30 21:23:22 -0400

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,6 +3,8 @@ class VisitorsController < ApplicationController
   def new
     #LearnRails.debug 'entering new method'
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
     #LearnRails.debug "Owner name is #{@owner.name}"
   end
 
