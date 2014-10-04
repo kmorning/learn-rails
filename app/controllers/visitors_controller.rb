fdc031aa084acf6e7ea29ddf480e9d2957363035
Kelly Morning <kleemorning@gmail.com>
2014-10-04 18:20:05 -0400

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,8 @@ class VisitorsController < ApplicationController
   def new
     #LearnRails.debug 'entering new method'
     @owner = Owner.new
-    flash.now[:notice] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
+    #flash.now[:notice] = 'Welcome!'
+    #flash.now[:alert] = 'My birthday is soon.'
     #LearnRails.debug "Owner name is #{@owner.name}"
   end
 
